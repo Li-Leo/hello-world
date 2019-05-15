b777449cfba3643bfc126e34d55439878ff9dbcc
ljlleo <ljl_leo@163.com>
2019-05-15 18:12:21 +0800

    111

--- test.rb
+++ test.rb
@@ -4,8 +4,11 @@ import time
 import sys, os
 
 def print_time():
+
     print(time.asctime())
-    print(time.localtime())
+    # print(time.localtime())
+    print(time.ctime())
+    print(time.time())
 
 if __name__ == '__main__':
     print_time()
