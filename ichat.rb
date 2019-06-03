1fd847e3ddf8731257fc880dcd41f1c44de65362
ljlleo <ljl_leo@163.com>
2019-06-03 19:57:17 +0800

    itchat

---
+++ ichat.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import itchat
+import os
+HELP_MSG = u'''
+availeble cmds:
+    clac
+    shutdown -xx(s)
+    cancle
+    notepad
+'''
+@itchat.msg_register(itchat.content.TEXT)
+def music_player(msg):
+    cmd = ''
+    if msg['ToUserName'] != 'filehelper': return
+    
+    if msg['Text'] == 'calc':
+        os.system("calc")
+    elif 'shutdown' in msg['Text']:
+        cmd = "shutdown /s /t " + (msg['Text'])[9:]
+        os.system(cmd)
+    elif msg['Text'] == 'cancle':
+        os.system("shutdown /a")
+    elif msg['Text'] == 'notepad':
+        os.system("notepad")
+    else:
+        itchat.send(msg['Text'], 'filehelper')
+
+
+itchat.auto_login(True)
+itchat.send(HELP_MSG, 'filehelper') 
+itchat.run()
